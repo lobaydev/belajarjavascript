41b36a74b9de7a6ad374a8f60dbbb7ba6a5871a7
Rizki Kurnia Putra <lobay.gd@gmail.com>
2023-06-29 20:07:25 +0700

    belajar variabel

--- 01_variabel/index.js
+++ 01_variabel/index.js
@@ -1,1 +1,34 @@
-cek
+// cara lama
+// function test(){
+//     for ( var i= 0; i < 10; i++){
+//         console.log(i);
+//     }
+// }
+// test();
+
+// for ( let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// const x = 10;
+// console.log(x);
+
+// const mhs = {
+//     nama: "Rizki",
+//     Umur: 28
+// }
+
+// console.log(mhs);
+
+
+// Let variabel yang bagus untuk looping karena nilainya bisa berubah
+// const adalah variabel dengan nilai yang tidak bisa dirubah, kalaupun bisa di rubah hanya dengan metode objek dengan seperti dibawah ini
+// const mhs = {
+//     nama: "Rizki",
+//     Umur: 28
+// }
+
+// mhs.Umur=27;
+// console.log(mhs);
+
+// Namun conts tidak akan error jika objek nya yang di rubah
